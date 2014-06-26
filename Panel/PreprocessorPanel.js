06f18bbe9174d5c294331ca6990d429539ecc231
Aleksey Kozyatinskiy <kozyatinskiy@google.com>
2014-06-26 20:55:02 +0400

    light-table init commit

--- Panel/PreprocessorPanel.js
+++ Panel/PreprocessorPanel.js
@@ -31,6 +31,7 @@ function preprocessorCoverage(source, url, listenerName) {
 		return location;
 	}
 
+<<<<<<< Updated upstream
 	var idToLocation = {};
 	var ast = esprima.parse(source, {loc:true});
 
@@ -62,12 +63,76 @@ function preprocessorCoverage(source, url, listenerName) {
 				++preprocessorCoverage.last_location_id;
 				idToLocation[preprocessorCoverage.last_location_id] = addSourceUrl(node.loc);
 				return {expressions: [makeInstrument(preprocessorCoverage.last_location_id), node], type: "SequenceExpression", old: node };				
+=======
+	var emptyLines = '';
+	for (var i = 0; i < startLine - 1; ++i) emptyLines += '\n';
+
+	var ast = esprima.parse(emptyLines + source, {loc:true});
+	var profiled_block = '\
+		function fake() {\n\
+			function __profiled__(/*here function arguments*/) {\n\
+				// here function body\n\
+			}\n\
+			if (window.top.__profileEnable) {\n\
+				try {\n\
+					window.top.__entryTime[++window.top.__entryTop] = window.top.performance.now();\n\
+					return __profiled__.apply(this, arguments);\n\
+				} finally {\n\
+					++window.top.__profileLast;\n\
+					window.top.__profileFinish[window.top.__profileLast] = window.top.performance.now();\n\
+					window.top.__profileStack[window.top.__profileLast] = window.top.__entryTop;\n\
+					window.top.__profileStart[window.top.__profileLast] = window.top.__entryTime[window.top.__entryTop--];\n\
+					window.top.__profileFunction[window.top.__profileLast] = 1 /* here correct function ID */;\n\
+				}\n\
+			}\n\
+			return __profiled__.apply(this, arguments);\n\
+		}\n\
+	';
+
+	if (url.length < 1)
+		url = 'data:application/javascript;base64,' + btoa(source);
+
+	estraverse.traverse(ast, {
+		enter: function (node, parent) {
+			// if (node.loc) {
+			// 	node.loc.start.line += startLine + 1;
+			// 	node.loc.end.line += startLine + 1;
+			// }
+			if ((node.type === "FunctionDeclaration" && node.id.name !== "__profiled__") || node.type === "FunctionExpression") {
+				preprocessor.functionID++;
+				if (node.id)
+					idToFunctionName[preprocessor.functionID] = node.id.name;
+				else if (node.type === "FunctionExpression" && parent && parent.type === "VariableDeclaration")
+					idToFunctionName[preprocessor.functionID] = parent.id.name;
+				else if (node.type === "FunctionExpression" && parent && parent.type === "Property")
+					idToFunctionName[preprocessor.functionID] = parent.key.name;
+				else {
+					idToFunctionName[preprocessor.functionID] = '(anonymous: ' + preprocessor.functionID.toString() + ')';
+				}
+				idToURL[preprocessor.functionID] = url ? url : '';
+				idToLocation[preprocessor.functionID] = (node.loc.start.line  ? node.loc.start.line : 1) + ',' + (node.loc.start.column ? node.loc.start.column : 1);
+
+				var profiled_ast = esprima.parse(profiled_block);
+				var profiled_function = profiled_ast.body[0].body.body[0];
+				// setup inner profiled function body
+				profiled_function.body = node.body;
+				// setup inner profiled function arguments
+				profiled_function.defaults = node.defaults;
+				profiled_function.params = node.params;
+				// setup function ID
+				var function_id_node = profiled_ast.body[0].body.body[1].consequent.body[0].finalizer.body[4].expression.right;
+				function_id_node.raw = preprocessor.functionID.toString();
+				function_id_node.value = preprocessor.functionID;
+				// setup alternative function body
+				node.body = profiled_ast.body[0].body;
+>>>>>>> Stashed changes
 			}
 		}
 	});
 	
 	var processed_source = escodegen.generate(ast);
 
+<<<<<<< Updated upstream
 	var prefix = '\
 window.top.__hits = window.top.__hits || new Int32Array(1024 * 1024);\n\
 window.top.__profileEnable = window.top.__profileEnable !== undefined ? window.top.__profileEnable : false;\n\
@@ -80,6 +145,74 @@ window.top.__urlToSource = window.top.__urlToSource || {};\n';
   	prefix = prefix.concat('window.top.__urlToSource[\'' + escape(url) + '\'] = \'' + escape(source) + '\';\n');
 
   	return '{\n' + prefix + '}\n' + processed_source;
+=======
+  	var prefix = '{\n\
+  		window.top.__entryTime = window.top.__entryTime || new Float64Array(4 * 4096);\n\
+  		window.top.__entryTop = window.top.__entryTop || -1;\n\
+  		window.top.__profileFunction = window.top.__profileFunction || new Int16Array(16 * 65536);\n\
+  		window.top.__profileStart = window.top.__profileStart || new Float64Array(16 * 65536);\n\
+  		window.top.__profileFinish = window.top.__profileFinish || new Float64Array(16 * 65536);\n\
+  		window.top.__profileLast = window.top.__profileLast || -1;\n\
+  		window.top.__profileStack = window.top.__profileStack || new Int16Array(16 * 65536);\n\
+  		window.top.__idToFunctionName = window.top.__idToFunctionName || {};\n\
+  		window.top.__idToLocation = window.top.__idToLocation || {};\n\
+  		window.top.__idToHost = window.top.__idToHost || {};\n\
+  		window.top.__idToURL = window.top.__idToURL || {};\n\
+  		window.top.__profileEnable = window.top.__profileEnable !== undefined ? window.top.__profileEnable : false;\n\
+  	}\n';
+  	for (id in idToFunctionName) {
+  		prefix = prefix.concat('window.top.__idToFunctionName[' + id + '] = \'' + idToFunctionName[id] + '\';\n');
+  		prefix = prefix.concat('window.top.__idToLocation[' + id + '] = \'' + idToLocation[id] + '\';\n');
+  		prefix = prefix.concat('window.top.__idToURL[' + id + '] =\'' + idToURL[id] + '\';\n');
+  		if (!url.startsWith('http://'))
+  			prefix = prefix.concat('window.top.__idToHost[' + id + '] = \'http://\' + window.location.host + \'/\';\n');
+  		else
+  			prefix = prefix.concat('window.top.__idToHost[' + id + '] = \'\';\n');
+  	}
+	prefix = prefix.concat('window.top.__idToFunctionName[-1] = \'preprocess\';\n');
+	prefix = prefix.concat('window.top.__idToURL[-1] = \'preprocess\';\n');
+	prefix = prefix.concat('window.top.__idToLocation[-1] = \'1,1\';\n');
+	prefix = prefix.concat('window.top.__idToHost[-1] = \'\';\n');
+
+  	var end_time = (new Date()).getTime();
+  	var total_time = (end_time - start_time);
+
+  	if (total_time > 0.0) {
+	  	prefix += '\
+	  		++window.top.__profileLast;\n\
+			window.top.__profileFinish[window.top.__profileLast] = window.performance.now();\n\
+			window.top.__profileStart[window.top.__profileLast] = window.top.__profileFinish[window.top.__profileLast] - ' + total_time.toString() + ';\n\
+			window.top.__profileStack[window.top.__profileLast] = -100;\n\
+			window.top.__profileFunction[window.top.__profileLast] = -1;\n\
+		';
+	}
+
+	var prefix_ast = esprima.parse(prefix);
+	var prefix_body = prefix_ast.body;
+
+	ast.body = prefix_body.concat(ast.body);
+	debugger;
+	var processed_result = escodegen.generate(ast, {sourceMap: url, sourceMapWithCode: true, sourceContent: source});
+	var processed_source = processed_result.code;
+
+	var lastPointIndex = url.lastIndexOf(".");
+	if (lastPointIndex !== -1)
+		url = url.substr(0, lastPointIndex) + '.profiled.' + url.substr(lastPointIndex + 1);
+	else
+		url = url + '.profiled';
+
+	console.log(processed_source + 
+			'\n//@ sourceMappingURL=data:application/json;base64,' + btoa(processed_result.map.toString()));
+
+	console.log(processed_result.map.toString());
+
+	if (processed_result.map)
+		return processed_source + 
+			'\n//@ sourceMappingURL=data:application/json;base64,' + btoa(processed_result.map.toString()) + 
+			'\n//@ sourceURL=' + url;
+	return '{\n' + prefix + '}\n' + processed_source;// + 
+		'\n//@ sourceURL=' + url;
+>>>>>>> Stashed changes
 }
 
 
@@ -308,6 +441,16 @@ function clearProfile() {
 }
 
 
+function testDataURI() {
+chrome.devtools.panels.sources.createSidebarPane("Selected variable",
+	function (sidebar) {
+		sidebar.setPage("Panel/VariablesValue.html");
+		// sidebar.setHeight("32ex");
+		console.log(sidebar.bodyElement);
+	});
+}
+
+
 function listen() {
   var refreshButton = document.querySelector('.refresh-button');
   refreshButton.addEventListener('click', refreshCoverage/*refreshTimeline*/);
@@ -321,7 +464,6 @@ function listen() {
   var reloadCoverageButton = document.querySelector('.reload-coverage-button');
   reloadCoverageButton.addEventListener('click', reloadWithCoverageAnalysis);
 }
-
 
 window.addEventListener('load', listen);
 
--- chrome-preprocessor.js
+++ chrome-preprocessor.js
@@ -5,9 +5,27 @@ chrome.devtools.panels.create(
     null // no callback needed
 );
 
-// add in Elements panel
-// chrome.devtools.panels.elements.createSidebarPane("Selected variable",
-// 	function (sidebar) {
-// 		sidebar.setPage("Panel/VariablesValue.html");
-// 		sidebar.setHeight("8ex");
-// 	});
+function addLiveVariable() {
+	function selectionGetted(selection) {
+		var ol = addLiveVariable.document.getElementById('live-variables');
+		var li = addLiveVariable.document.createElement('li');
+		li.innerHTML = JSON.stringify(selection);
+		ol.appendChild(li);
+	}
+
+	chrome.devtools.panels.getSelectedCode(selectionGetted);	
+}
+
+chrome.devtools.panels.sources.createSidebarPane("Selected variable",
+	function (sidebar) {
+		sidebar.setPage("Panel/VariablesValue.html");
+		sidebar.onShown.addListener(function (frame) {
+  			var addLiveVariableButton = frame.document.querySelector('.add-live-variable');
+  			addLiveVariableButton.addEventListener('click', addLiveVariable);
+  			addLiveVariable.document = frame.document;
+
+			console.log(frame);
+		});
+		// sidebar.setHeight("32ex");
+		console.log(sidebar.bodyElement);
+	});
