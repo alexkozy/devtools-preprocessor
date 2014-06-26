45c1513f659a4b86c0689b63d54d52675fb41734
Aleksey Kozyatinskiy <kozyatinskiy@google.com>
2014-06-26 15:43:49 +0400

    123

--- chrome-preprocessor.js
+++ chrome-preprocessor.js
@@ -4,3 +4,10 @@ chrome.devtools.panels.create(
     'Panel/PreprocessorPanel.html',
     null // no callback needed
 );
+
+// add in Elements panel
+// chrome.devtools.panels.elements.createSidebarPane("Selected variable",
+// 	function (sidebar) {
+// 		sidebar.setPage("Panel/VariablesValue.html");
+// 		sidebar.setHeight("8ex");
+// 	});
