56e1758bcb698b903fd6d94f53508609517530e4
Aleksey Kozyatinskiy <kozyatinskiy@google.com>
2014-07-05 19:32:03 +0400

    refactoring in progress

--- Panel/PreprocessorPanel.js
+++ Panel/PreprocessorPanel.js
@@ -1,160 +1,6 @@
 // /usr/local/google/home/k…/src/third_party/WebKit/Source
 (function() {
 
-// This function is converted to a string and becomes the preprocessor
-function preprocessor(source, url, listenerName, startLine) {
-	console.log(url);
-	console.log(startLine);
-
-	var start_time = (new Date()).getTime();
-
-	if (preprocessor.esprima === undefined) {
-		__PREFIX__;
-		sourceMap = window.sourceMap;
-	} else {
-		esprima = preprocessor.esprima;
-		estraverse = preprocessor.estraverse;
-		escodegen = preprocessor.escodegen;
-		sourceMap = preprocessor.sourceMap;
-	}
-	preprocessor.esprima = esprima;
-	preprocessor.estraverse = estraverse;
-	preprocessor.escodegen = escodegen;
-	preprocessor.sourceMap = sourceMap;
-	if (preprocessor.functionID === undefined) preprocessor.functionID = 0;
-	var idToFunctionName = {};
-	var idToLocation = {};
-	var idToURL = {};
-	String.prototype.startsWith = function (str){
-	    return this.indexOf(str) == 0;
-	};
-
-	var ast = esprima.parse(source + '\n\n\n\n', {loc:true});
-	var profiled_block = '\
-		function fake() {\n\
-			function __profiled__(/*here function arguments*/) {\n\
-				// here function body\n\
-			}\n\
-			if (window.top.__profileEnable) {\n\
-				try {\n\
-					window.top.__entryTime[++window.top.__entryTop] = window.top.performance.now();\n\
-					return __profiled__.apply(this, arguments);\n\
-				} finally {\n\
-					++window.top.__profileLast;\n\
-					window.top.__profileFinish[window.top.__profileLast] = window.top.performance.now();\n\
-					window.top.__profileStack[window.top.__profileLast] = window.top.__entryTop;\n\
-					window.top.__profileStart[window.top.__profileLast] = window.top.__entryTime[window.top.__entryTop--];\n\
-					window.top.__profileFunction[window.top.__profileLast] = 1 /* here correct function ID */;\n\
-				}\n\
-			}\n\
-			return __profiled__.apply(this, arguments);\n\
-		}\n\
-	';
-
-	if (url.length < 1)
-		url = 'data:application/javascript;base64,' + btoa(source);
-
-	estraverse.traverse(ast, {
-		enter: function (node, parent) {
-			// if (node.loc) {
-				// node.loc.start.line += startLine + 1;
-				// node.loc.end.line += startLine + 1;
-			// }
-			if ((node.type === "FunctionDeclaration" && node.id.name !== "__profiled__") || node.type === "FunctionExpression") {
-				preprocessor.functionID++;
-				if (node.id)
-					idToFunctionName[preprocessor.functionID] = node.id.name;
-				else if (node.type === "FunctionExpression" && parent && parent.type === "VariableDeclaration")
-					idToFunctionName[preprocessor.functionID] = parent.id.name;
-				else if (node.type === "FunctionExpression" && parent && parent.type === "Property")
-					idToFunctionName[preprocessor.functionID] = parent.key.name;
-				else {
-					idToFunctionName[preprocessor.functionID] = '(anonymous: ' + preprocessor.functionID.toString() + ')';
-				}
-				idToURL[preprocessor.functionID] = url ? url : '';
-				idToLocation[preprocessor.functionID] = (node.loc.start.line  ? node.loc.start.line : 1) + ',' + (node.loc.start.column ? node.loc.start.column : 1);
-
-				var profiled_ast = esprima.parse(profiled_block);
-				var profiled_function = profiled_ast.body[0].body.body[0];
-				// setup inner profiled function body
-				profiled_function.body = node.body;
-				// setup inner profiled function arguments
-				profiled_function.defaults = node.defaults;
-				profiled_function.params = node.params;
-				// setup function ID
-				var function_id_node = profiled_ast.body[0].body.body[1].consequent.body[0].finalizer.body[4].expression.right;
-				function_id_node.raw = preprocessor.functionID.toString();
-				function_id_node.value = preprocessor.functionID;
-				// setup alternative function body
-				node.body = profiled_ast.body[0].body;
-			}
-		}
-	});
-
-  	var prefix = '\
-  		window.top.__entryTime = window.top.__entryTime || new Float64Array(4 * 4096);\n\
-  		window.top.__entryTop = window.top.__entryTop || -1;\n\
-  		window.top.__profileFunction = window.top.__profileFunction || new Int16Array(16 * 65536);\n\
-  		window.top.__profileStart = window.top.__profileStart || new Float64Array(16 * 65536);\n\
-  		window.top.__profileFinish = window.top.__profileFinish || new Float64Array(16 * 65536);\n\
-  		window.top.__profileLast = window.top.__profileLast || -1;\n\
-  		window.top.__profileStack = window.top.__profileStack || new Int16Array(16 * 65536);\n\
-  		window.top.__idToFunctionName = window.top.__idToFunctionName || {};\n\
-  		window.top.__idToLocation = window.top.__idToLocation || {};\n\
-  		window.top.__idToHost = window.top.__idToHost || {};\n\
-  		window.top.__idToURL = window.top.__idToURL || {};\n\
-  		window.top.__profileEnable = window.top.__profileEnable !== undefined ? window.top.__profileEnable : false;\n\
-  	';
-  	for (id in idToFunctionName) {
-  		prefix = prefix.concat('window.top.__idToFunctionName[' + id + '] = \'' + idToFunctionName[id] + '\';\n');
-  		prefix = prefix.concat('window.top.__idToLocation[' + id + '] = \'' + idToLocation[id] + '\';\n');
-  		prefix = prefix.concat('window.top.__idToURL[' + id + '] =\'' + idToURL[id] + '\';\n');
-  		if (!url.startsWith('http://'))
-  			prefix = prefix.concat('window.top.__idToHost[' + id + '] = \'http://\' + window.location.host + \'/\';\n');
-  		else
-  			prefix = prefix.concat('window.top.__idToHost[' + id + '] = \'\';\n');
-  	}
-	prefix = prefix.concat('window.top.__idToFunctionName[-1] = \'preprocess\';\n');
-	prefix = prefix.concat('window.top.__idToURL[-1] = \'preprocess\';\n');
-	prefix = prefix.concat('window.top.__idToLocation[-1] = \'1,1\';\n');
-	prefix = prefix.concat('window.top.__idToHost[-1] = \'\';\n');
-
-  	var end_time = (new Date()).getTime();
-  	var total_time = (end_time - start_time);
-
-  	if (total_time > 0.0) {
-	  	prefix += '\
-	  		++window.top.__profileLast;\n\
-			window.top.__profileFinish[window.top.__profileLast] = window.performance.now();\n\
-			window.top.__profileStart[window.top.__profileLast] = window.top.__profileFinish[window.top.__profileLast] - ' + total_time.toString() + ';\n\
-			window.top.__profileStack[window.top.__profileLast] = -100;\n\
-			window.top.__profileFunction[window.top.__profileLast] = -1;\n\
-		';
-	}
-
-	var prefix_ast = esprima.parse(prefix);
-	var prefix_body = prefix_ast.body;
-
-	ast.body = prefix_body.concat(ast.body);
-
-	var processed_result = escodegen.generate(ast, {sourceMap: url, sourceMapWithCode: true, sourceContent: source});
-	var processed_source = processed_result.code;
-
-	var lastPointIndex = url.lastIndexOf(".");
-	if (lastPointIndex !== -1)
-		url = url.substr(0, lastPointIndex) + '.profiled.' + url.substr(lastPointIndex + 1);
-	else
-		url = url + '.profiled';
-
-	if (processed_result.map)
-		return processed_source + 
-			'\n//@ sourceMappingURL=data:application/json;base64,' + btoa(processed_result.map.toString()) + 
-			'\n//@ sourceURL=' + url;
-	return '{\n' + prefix + '}\n' + processed_source;// + 
-		'\n//@ sourceURL=' + url;
-}
-
-
 function XHRLoadFile(name, path) {
 	console.log('Loading lib: ' + name + '..');
 	var req = new XMLHttpRequest();
@@ -184,15 +30,15 @@ function preprocessorWithLibs(preprocessor_source, libs) {
 }
 
 
-function reloadWithPreprocessor(injectedScript) {
-	var preprocessor_source = preprocessorWithLibs(preprocessor.toString(), ['esprima.js', 'estraverse.js', 'escodegen.browser.js', 'source-map.js']);
+function reloadWithPreprocessor(preprocessor) {
+	console.log(preprocessor.preprocessor.toString());
+	console.log(preprocessor.injectedScript.toString() + '__beforeAll()');
   	var options = {
     	ignoreCache: true,
     	userAgent: undefined,
-    	injectedScript: undefined,
-    	preprocessingScript: preprocessor_source
+    	injectedScript: preprocessor.injectedScript.toString() + '__beforeAll()',
+    	preprocessingScript: preprocessor.preprocessorWithLibs()
   	};
-
   	chrome.devtools.inspectedWindow.reload(options);
 }
 
@@ -299,9 +145,7 @@ function removePreprocessFromReport(report) {
 
 
 function demoPreprocessor() {
-	function onLoaded() {}
-	var loadMonitor = new InspectedWindow.LoadMonitor(onLoaded);
-	reloadWithPreprocessor(loadMonitor.injectedScript);
+	reloadWithPreprocessor(new Profiler());
 }
 
 
@@ -311,7 +155,7 @@ function refreshTimeline() {
 		if (isException)
 			throw new Error('Eval failed for ' + expr, isException.value);
 		document.getElementById('results_count').innerHTML = 'Total count: ' + report.length.toString();
-		refreshVisual(removePreprocessFromReport(report));	
+		refreshVisual(removePreprocessFromReport(report));
 	}
 	chrome.devtools.inspectedWindow.eval(expr, onEval);
 }
@@ -360,7 +204,7 @@ function switchProfiler() {
 	function onEval(profileEnable, isException) {
 		if (isException)
 			throw new Error('Eval failed for ' + expr, isException.value);
-		
+
 		var newValue = !profileEnable;
 		var newInner = newValue ? 'Disable' : 'Enable';
 
@@ -370,7 +214,7 @@ function switchProfiler() {
 		if (!newValue)
 			refreshTimeline();
 	}
-	chrome.devtools.inspectedWindow.eval(expr, onEval);		
+	chrome.devtools.inspectedWindow.eval(expr, onEval);
 }
 
 
@@ -379,10 +223,10 @@ function clearProfile() {
 	function onEval(res, isException) {
 		if (isException)
 			throw new Error('Eval failed for ' + expr, isException.value);
-		
+
 		refreshTimeline();
 	}
-	chrome.devtools.inspectedWindow.eval(expr, onEval);			
+	chrome.devtools.inspectedWindow.eval(expr, onEval);
 }
 
 
