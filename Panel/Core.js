15ffa21380e6af2c9613452d5536e90a7ba03dcc
Aleksey Kozyatinskiy <ak239spb@gmail.com>
2014-10-01 01:02:56 +0400

    Add LICENSE and license headers

--- Panel/Core.js
+++ Panel/Core.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 function Core(title_span) {
     this.current_ = "";
     this.title_span_ = title_span;
